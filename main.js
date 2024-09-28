95dabbd6a3981148f01bb73677b92f754e632e22
thanhtrung102 <119796817+thanhtrung102@users.noreply.github.com>
2024-09-28 18:56:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -41,19 +41,6 @@ let track_list = [
   },
 ];
 
-function random_bg_color() {
-
-  // Get a number between 64 to 256 (for getting lighter colors)
-  let red = Math.floor(Math.random() * 256) + 64;
-  let green = Math.floor(Math.random() * 256) + 64;
-  let blue = Math.floor(Math.random() * 256) + 64;
-
-  // Construct a color withe the given values
-  let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
-
-  // Set the background to that color
-  document.body.style.background = bgColor;
-}
 
 function loadTrack(track_index) {
   clearInterval(updateTimer);
@@ -68,7 +55,6 @@ function loadTrack(track_index) {
 
   updateTimer = setInterval(seekUpdate, 1000);
   curr_track.addEventListener("ended", nextTrack);
-  random_bg_color();
 }
 
 function resetValues() {
